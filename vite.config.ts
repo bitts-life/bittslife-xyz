cbb95d547a3a9ba271861ef2a67ecf82115797b9
Shubham Sharma <shubham.sharma12@walmart.com>
2025-06-18 18:25:44 +0530

    commit1

--- vite.config.ts
+++ vite.config.ts
@@ -10,11 +10,10 @@ export default defineConfig(({ mode }) => ({
   //   port: 8080,
   // },
   plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  base: "/bittslife-xyz",
+    react()],
+    // mode === 'development' &&
+    // componentTagger(),
+  // ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
