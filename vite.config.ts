3311506880d8b4521e0983e530e892e3a4790f80
Shubham Sharma <shubham.sharma12@walmart.com>
2025-06-18 18:08:05 +0530

    code updated

--- vite.config.ts
+++ vite.config.ts
@@ -11,13 +11,13 @@ export default defineConfig(({ mode }) => ({
   // },
   plugins: [
     react(),
-    // mode === 'development' &&
-    // componentTagger(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   base: "/bittslife-xyz",
-  // resolve: {
-  //   alias: {
-  //     "@": path.resolve(__dirname, "./src"),
-  //   },
-  // },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 }));
