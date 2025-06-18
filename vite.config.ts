5fdee00666d3f09bfb7ca3fa2ed3e6e1031b8b38
Shubham Sharma <shubham.sharma12@walmart.com>
2025-06-18 18:05:10 +0530

    commented unnecessory code

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,19 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  // server: {
+  //   host: "::",
+  //   port: 8080,
+  // },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    // mode === 'development' &&
+    // componentTagger(),
   ].filter(Boolean),
   base: "/bittslife-xyz",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //   },
+  // },
 }));
