c81b6c69fe9cbe1c5853a0bbf8e9717baa3571cd
Shubham Sharma <shubham.sharma12@walmart.com>
2025-06-19 22:32:45 +0530

    base changed

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react()
   ],
-  base: "/bittslife-xyz/",
+  base: "/",
     // mode === 'development' &&
     // componentTagger(),
   // ].filter(Boolean),
